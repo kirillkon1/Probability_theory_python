9c31df49c66a233707cd3f19ac0da235d33fb874
LeartAule <kirill.kondrashow@mail.ru>
2021-05-09 15:08:00 +0300

    baobab

--- Theor1/main.py
+++ Theor1/main.py
@@ -7,17 +7,7 @@ from Number_class import NumberWithProbability, printTable, toFixed, drawFunctio
 digits_list = [-0.76, -0.55, -0.62, 0.21, -1.31, 0.64, -0.21, -1.07, 0.21, 1.16,
                -1.14, 1.07, -0.14, -1.45, 1.45, 0.24, 1.46, 1.04, -0.31, -1.12]
 
-# digits_list = [-0.76, -0.55, -0.62, 0.21, -1.31, 0.64, -0.21, -1.07, 0.21, 1.16,
-#                -1.14, 1.07, -0.14, -1.45, 1.45, 0.24, 1.46, 1.04, -0.31, -1.12,
-#                1.13, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-
 digits_set = set(digits_list)
-
-
-def print_mas(l: list):
-    for i in l:
-        print(i)
-
 
 if __name__ == '__main__':
     # ------ вариационный ряд -------
@@ -34,6 +24,8 @@ if __name__ == '__main__':
     # --- Размах выборки ----
 
     R = maximum - minimum
+
+    print(f"Размах = {R}")
 
     # --- Эмпирическая фун-ция распределения ---
     counter = Counter(digits_list)
@@ -54,6 +46,10 @@ if __name__ == '__main__':
     dispersion = math_sqr_expectation - math_expectation ** 2  # M(x^2) - M(x)
     standard_deviation = math.sqrt(dispersion)
 
+    print(f"Математическое ожидание = {toFixed(math_expectation)}\n"
+          f"Дисперсия = {toFixed(dispersion)}\n"
+          f"СКО = {toFixed(standard_deviation)}\n")
+
     drawFunction(getPointForStaticGraphic(number_list), "полигон приведенных частот")
     drawFunction(getEmpiricalFunction(number_list, False), "график эмпирической фукнции распределения")
 
@@ -61,13 +57,13 @@ if __name__ == '__main__':
     h = float(toFixed((maximum - minimum) / (1 + math.log(len(number_list), 2))))
 
     x = minimum - h / 2
-    bbb = []
+    border_list = []
     print(h)
     while x <= maximum:
-        bbb.append(x)
+        border_list.append(x)
         x += h
     else:
-        bbb.append(x)
-    preGraphList = preGraphFunction(digits_list, bbb)
+        border_list.append(x)
+    preGraphList = preGraphFunction(digits_list, border_list)
 
-    drawBarGraph(digits_list, bbb)
+    drawBarGraph(digits_list, border_list)
